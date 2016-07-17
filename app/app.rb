c70d5be0f573582444a78391d80847beb11495ad
neodevelop <juan@makingdevs.com>
2016-07-17 14:35:27 -0500

    Adding sinatra app

---
+++ app/app.rb
@@ -0,0 +1,32 @@
+# app/app.rb
+
+# install and require all dependencies
+require 'sinatra/base'
+require 'sprockets'
+require 'uglifier'
+require 'sass'
+require 'coffee-script'
+require 'execjs'
+
+class MyApp < Sinatra::Base
+  # initialize new sprockets environment
+  set :environment, Sprockets::Environment.new
+
+  # append assets paths
+  environment.append_path "assets/stylesheets"
+  environment.append_path "assets/javascripts"
+
+  # compress assets
+  environment.js_compressor  = :uglify
+  environment.css_compressor = :scss
+
+  # get assets
+  get "/assets/*" do
+    env["PATH_INFO"].sub!("/assets", "")
+    settings.environment.call(env)
+  end
+
+  get "/" do
+    erb :index
+  end
+end
